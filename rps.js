d4bc0a0cd0225e8abf9d4eee599ac11f3b71d860
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-13 02:05:20 -0600

    Create the pseudocode for the project

---
+++ rps.js
@@ -0,0 +1,9 @@
+// Create function called getComputerChoice() that randomly chooses rock, paper or scissors and return the value.
+  // Create an array with rock, paper and scissor in it.
+  // Create a variable that generates a random number 0-2 and obtains the item in that position within the array
+
+// Create a function that takes two parameters, playerSelection and computerSelection, and then returns the winner or a tie.
+  // Function should be case insensitive to avoid unexpected errors. 
+  // If there is a tie, replay the round.
+
+// Write a function called game() to play a best-of-five game. 
