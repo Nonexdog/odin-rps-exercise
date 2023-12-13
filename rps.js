0f19ac5f4d10be8236764df107718bf1dbff5b88
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-13 02:11:50 -0600

    Create function to generate a random choice for rps
    The function creates an array with three choices, generates a random
    value between 0-2 and returns the element in that index.

--- rps.js
+++ rps.js
@@ -2,6 +2,14 @@
   // Create an array with rock, paper and scissor in it.
   // Create a variable that generates a random number 0-2 and obtains the item in that position within the array
 
+function getComputerChoice() {
+  const RPS = ['rock','paper','scissors'];
+  // Get a random number between 0 and 2 inclusive
+  const randomSelection = Math.floor(Math.random()*3);
+  return RPS.at(randomSelection);
+}
+console.log('Computer choice: ' + getComputerChoice());
+
 // Create a function that takes two parameters, playerSelection and computerSelection, and then returns the winner or a tie.
   // Function should be case insensitive to avoid unexpected errors. 
   // If there is a tie, replay the round.
