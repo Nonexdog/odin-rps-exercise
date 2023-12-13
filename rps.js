ef369b567bfd8040edb8e3dc1f3b30183fbd71eb
Nonexdog <63328613+Nonexdog@users.noreply.github.com>
2023-12-13 02:41:14 -0600

    Add function to choose winner between user and CPU
    This function is a little proof-of-concept for the system and will end
    up being reworked for the final project.

--- rps.js
+++ rps.js
@@ -14,4 +14,57 @@ console.log('Computer choice: ' + getComputerChoice());
   // Function should be case insensitive to avoid unexpected errors. 
   // If there is a tie, replay the round.
 
+function pickWinner(playerSelection, computerSelection) {
+  // Declare empty winner variable and turn player input into lowercase
+  let winner;
+  playerSelection = playerSelection.toLowerCase();
+  // Switch that compares values inputted by the player and the CPU. If there is a tie or an invalid value is typed in, repeat
+  switch (playerSelection) {
+    case 'rock':
+      if (computerSelection === 'scissors') {
+        winner = 'Player';
+        break;
+      } else if (computerSelection === 'paper') {
+        winner = 'Computer';
+        break;
+      } else {
+        break;
+      }
+    case 'paper':
+      if (computerSelection === 'rock') {
+        winner = 'Player';
+        break;
+      } else if (computerSelection === 'scissors') {
+        winner = 'Computer';
+        break;
+      } else {
+        break;
+      }
+    case 'scissors':
+      if (computerSelection === 'paper') {
+        winner = 'Player';
+        break;
+      } else if (computerSelection === 'rock') {
+        winner = 'Computer';
+        break;
+      } else {
+        break;
+      }
+    default:
+      return 'Hey!! That is not a valid selection!!';
+  }
+
+  if (winner === 'Player') {
+    return `${winner} is the winner!! ${playerSelection} beats ${computerSelection}`;
+  } else if (winner === 'Computer') {
+    return `${winner} is the winner!! ${computerSelection} beats ${playerSelection}`;
+  } else {
+    return 'It\'s a tie!!';
+  }
+}
+
+console.log(pickWinner('ROCK','paper'));
+console.log(pickWinner('scISsors','paper'));
+console.log(pickWinner('paper','paper'));
+
 // Write a function called game() to play a best-of-five game. 
